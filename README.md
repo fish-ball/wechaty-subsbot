369af6121cd422f953976a2617b7b3942d385e72
fish-ball <57082212@qq.com>
2020-05-11 12:54:18 +0800

    Initialize

---
+++ README.md
@@ -0,0 +1,59 @@
+Wechaty 订阅机器人 [![Powered by Wechaty](https://img.shields.io/badge/Powered%20By-Wechaty-green.svg)](https://github.com/chatie/wechaty)[![Wechaty开源激励计划](https://img.shields.io/badge/Wechaty-开源激励计划-green.svg)](https://github.com/juzibot/Welcome/wiki/Everything-about-Wechaty)
+==================
+
+需求
+----
+
+想实现一个在微信上可以订阅主流媒体平台的频道（初步实现 bilibili，后面可以扩展甚至微博之类的信息源），
+如果频道有更新，即可通过机器人第一时间将发布的媒体推送给订阅的用户。
+
+## 1.0. 如何安装
+
+1. clone 本项目
+
+```
+git clone https://github.com/fish-ball/wechaty-subsbot
+```
+
+2. 准备好 Node.js 运行环境
+
+3. 安装 typescript 和 ts-node
+
+```
+npm i -g typescript ts-node
+```
+
+4. 安装项目依赖
+
+```
+npm i
+```
+
+5. 填写配置
+
+将 `config.ts.example` 复制为 `config.ts`，并且填写申请到的 token，暂时只支持 padplus。
+
+6. 运行启动脚本
+
+```
+ts-node bot.ts
+```
+
+## 2.0. 如何使用
+
+1. 第一次运行启动时 console 会显示登录的二维码，用机器人的微信扫码登录，登录成功机器人即已经上线工作。
+
+2. 其他用户添加机器人为好友，向其转发B站up主的页面，（直接微信转发或者复制链接再发都行），即可订阅这个UP主的频道。
+
+3. 在订阅生效之后只要 UP 主新发了视频，就可以马上收到机器人的推送。
+
+4. 在与机器人聊天中输入指令 `:list` 可以查询到目前已订阅的频道列表，显示这些频道的 id 和名称。
+
+5. 如果需要退订，输入指令 `:leave <id>` 即可退订 id 对应的频道。
+
+## DEMO
+
+测试机器人(wx_easecloud)：
+
+[![Wechaty开源激励计划](wx_easecloud.jpg)]
+
---
+++ bot.ts
@@ -0,0 +1,179 @@
+import {Contact, UrlLink, Wechaty} from 'wechaty';
+import {MessageType, ScanStatus} from 'wechaty-puppet';
+import {PuppetPadplus} from 'wechaty-puppet-padplus';
+import QrcodeTerminal from 'qrcode-terminal';
+import fs from 'fs';
+import path from 'path';
+import fetch from 'node-fetch';
+import config from './config';
+
+const token = config.token;
+
+const puppet = new PuppetPadplus({
+  token,
+});
+
+const bot = new Wechaty({
+  puppet,
+  name: config.name, // generate xxxx.memory-card.json and save login data for the next login
+});
+
+bot.on('scan', (qrcode, status) => {
+  if (status === ScanStatus.Waiting) {
+    console.log(qrcode);
+    QrcodeTerminal.generate(qrcode, {
+      small: true,
+    });
+  }
+}).on('message', async msg => {
+  const text = msg.text();
+  const sender = msg.from();
+  if (!sender) return;
+
+  const ctx = new UserContext(sender.id);
+  ctx.data.subscriptions = ctx.data.subscriptions || {};
+
+  // 查询指令
+  if (/^:list$/.test(msg.content())) {
+    await ctx.getSubscriptionList();
+    return;
+  }
+
+  // 退订指令
+  if (/^:leave \d+$/.test(msg.content())) {
+    await ctx.leaveChannel(msg.content().split(' ')[1]);
+    return;
+  }
+
+  // console.log(msg.from());
+  // 只关注单聊收到的信息
+  if (msg.room()) {
+    console.log(msg);
+    return;
+  }
+
+  const match = /https:\/\/space\.bilibili\.com\/(\d+)/.exec(text);
+  if (!match) return;
+  const channelId = match[1];
+
+  // console.log(sender.id);
+  ctx.data.subscriptions[channelId] = ctx.data.subscriptions[channelId] || {};
+  const channel = ctx.data.subscriptions[channelId];
+  channel.lastTimestamp = Number(new Date()) / 1000;
+
+  // 加载频道名称
+  const url = `https://api.bilibili.com/x/space/acc/info?mid=${channelId}&jsonp=jsonp`;
+  const resp = await fetch(url);
+  const channelInfo = JSON.parse(await resp.text());
+  channel.name = channelInfo.data.name;
+
+  await sender!.say(`你已成功订阅《${channel.name}》
+:list - 查看当前订阅
+:leave <id> - 退订指定频道`);
+
+  console.log(ctx.data);
+  await ctx.save();
+}).on('login', async () => {
+  // 每分钟查一下有没有新片发布
+  setTimeout(async () => {
+    await SubscriptionRunner.check();
+  }, 60000);
+}).start();
+
+class UserContext {
+  userId: string;
+  fname: string;
+  data: any = {};
+
+  constructor(userId: string) {
+    this.userId = userId;
+    this.fname = `data/${userId}.subs`;
+    fs.mkdirSync(path.dirname(this.fname), {recursive: true, mode: 0o644});
+    try {
+      const content = fs.readFileSync(this.fname);
+      this.data = JSON.parse(content && content.toString() || '{}');
+      console.log(this.data);
+    } catch (err) {
+      if (err.code !== 'ENOENT') {
+        throw err;
+      }
+    }
+  }
+
+  async user() {
+    return bot.Contact.find({id: this.userId});
+  }
+
+  async save() {
+    // 获取
+    await fs.promises.writeFile(this.fname, JSON.stringify(this.data));
+    console.log('write ok');
+  }
+
+  async getSubscriptionList() {
+    const content = Object.entries(this.data.subscriptions || {}).map(([channelId, channelInfo]) => {
+      return `${channelId} - ${(channelInfo as any).name}`;
+    }).join('\n');
+    (await this.user())?.say(content);
+  }
+
+  async leaveChannel(channelId: string) {
+    // console.log(this.data.subscriptions);
+    const channel = this.data.subscriptions && this.data.subscriptions[channelId];
+    if (!channel) {
+      (await this.user())?.say('您没有订阅这个频道，请检查 ID 是否输入正确');
+    }
+    delete this.data.subscriptions[channelId];
+    await this.save();
+    (await this.user())?.say(`您已成功退订《${channel.name}》`);
+  }
+
+  static getUserIdList() {
+    try {
+      const files = fs.readdirSync('data');
+      return files.filter(f => /\.subs$/.test(f))
+        .map(f => f.replace(/\.subs/, ''));
+    } catch (err) {
+      if (err.code !== 'ENOENT') {
+        throw err;
+      }
+      return [];
+    }
+  }
+
+}
+
+// 定时获取订阅的 UP 主的视频，如果发现更新则推送
+class SubscriptionRunner {
+  static check() {
+    UserContext.getUserIdList().forEach(async userId => {
+      console.log(userId);
+      const ctx = new UserContext(userId);
+      const contact = await ctx.user();
+      if (!contact) return;
+      // console.log(ctx.data);
+      await Promise.all(Object.entries(ctx.data.subscriptions || {}).map(async ([channelId, channelInfo]) => {
+        const url = `https://api.bilibili.com/x/space/arc/search?mid=${channelId}&ps=30&tid=0&pn=1&keyword=&order=pubdate&jsonp=jsonp`;
+        const resp = await fetch(url);
+        const data = JSON.parse(await resp.text());
+        // console.log(data);
+        await Promise.all(data.data.list.vlist.map(async (video: any) => {
+          if (video.created > (channelInfo as any).lastTimestamp || 0) {
+            const linkPayload = new UrlLink({
+              description: video.description,
+              thumbnailUrl: video.pic.replace(/^\/\//, 'https://'),
+              title: video.title,
+              url: `https://bilibili.com/video/${video.bvid}`,
+            });
+            contact.say(linkPayload);
+            // 为了避免因为时间不一致导致重复更新，使用最新的时间点来更新最近时间
+            (channelInfo as any).lastTimestamp = video.created;
+          }
+        }));
+      }));
+      // 保存所有 ctx 的信息（主要是更新 lastTimestamp 时间）
+      await ctx.save();
+    });
+  }
+}
+
